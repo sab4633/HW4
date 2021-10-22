225fd0d0a1dce3cba81bf53b41f975ffddbf4907
Sofia Bzhilyanskaya <sab4633@rit.edu>
2021-10-22 12:36:25 -0400

    Added allwhitesquare

---
+++ src/AllWhiteSquare.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class AllWhiteSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[][] mat = new char[n][n];
+        int[][] opt = new int[n][n];
+        int max = 0;
+        sc.nextLine();
+        for (int i = 0; i < n; i++) { //iterate through input placing each number into the array;
+            String row = sc.nextLine();
+            char[] rowChar = row.toCharArray();
+            for(int j =0; j<n; j++){
+                mat[i][j] = rowChar[j];
+            }
+
+
+        }
+        for (int i = 0; i < n; i++) { //iterate through input placing each number into the array;
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 'b') {
+                    opt[i][j] = 0;
+                } else {
+                    int min = n;
+                    if (i != 0) {
+                        if (opt[i - 1][j] < min) {
+                            min = opt[i - 1][j];
+                        }
+
+                        if (j != 0) {
+                            if (opt[i][j - 1] < min) {
+                                min = opt[i][j - 1];
+                            }
+                        }
+                        if (i != 0 && j != 0) {
+                            if (opt[i - 1][j - 1] < min) {
+                                min = opt[i - 1][j - 1];
+                            }
+                        }
+                        if((i == 0 || j == 0)){
+                            min = 0;
+                        }
+                        opt[i][j] = min + 1;
+                        if (opt[i][j] > max) {
+                            System.out.println(i+" "+j);
+                            max = opt[i][j];
+                        }
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i < n; i++) { //iterate through input placing each number into the array;
+            for (int j = 0; j < n; j++) {
+                System.out.print(opt[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(max);
+    }
+
+}
--- src/KnapsackWeightBoundsWithSolution.java
+++ src/KnapsackWeightBoundsWithSolution.java
@@ -12,29 +12,67 @@ public class KnapsackWeightBoundsWithSolution {
             weights[i] = sc.nextInt();
             costs[i] = sc.nextInt();
         }
+        int diff = w2-w1;
         int[][] opt = new int[n+1][w2+1];
+        int[][] act = new int[n+1][w2+1];
         for(int i = 0; i<=w2; i++){
             opt[0][i] = 0;
+            act[0][i] = 0;
         }
         for(int i = 0; i<=n; i++){
             opt[i][0] = 0;
+            act[i][0] = 0;
         }
+        int maxVal = 0;
         for(int v = 1; v<=w2;v++){
             for(int j= 1;j<=n;j++){
                 opt[j][v] = opt[j-1][v];
                 if(weights[j-1]<=v && opt[j-1][v-weights[j-1]]+costs[j-1] > opt[j][v]){
+
                     opt[j][v] = opt[j-1][v-weights[j-1]]+costs[j-1];
 
+
+
                 }
+                System.out.println(j);
+                if (v >= w1 && opt[j][v] > maxVal) {
+                    maxVal = opt[j][v];//store the minimum cost when the weight is >= minW
+                }
+
+
             }
         }
 
-//        for(int i =0; i<=n; i++){
-//            for(int j=0; j<=w2;j++){
-//                System.out.print(opt[i][j]+" ");
-//            }
-//            System.out.println();
-//        }
-        System.out.println(opt[n][w2]);
+        System.out.println();
+        for(int i =0; i<=n; i++){
+            for(int j=0; j<=w2;j++){
+                System.out.print(opt[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("MAX"+maxVal);
+
+        int weight = w2;
+        int j = n;
+        int c = opt[j][weight];
+        while(weight>0 && j>=0){
+            while(j>=0 && opt[j][weight] == c ){
+                j--;
+            }
+            if(j>=0){
+                System.out.println((j+1)+" "+weights[j]);
+                c = opt[j][weight-weights[j]];
+
+                weight = weight-weights[j];
+            }else if(j==-1){
+                j++;
+
+                weight = weight-weights[j];
+            }
+
+
+        }
+        System.out.println(weight);
+
     }
 }
